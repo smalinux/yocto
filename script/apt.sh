dea64f6f9ddec796b45774fda2d1443560151e29
Sohaib Mohamed <sohaib.amhmd@gmail.com>
2024-10-19 16:31:41 +0200

    `apt.sh`: add tftpd-hpa
    Signed-off-by: Sohaib Mohamed <sohaib.amhmd@gmail.com>

--- script/apt.sh
+++ script/apt.sh
@@ -6,3 +6,7 @@
 
 yes | sudo apt install gawk wget git diffstat unzip texinfo gcc build-essential chrpath socat cpio python3 python3-pip python3-pexpect xz-utils debianutils iputils-ping python3-git python3-jinja2 python3-subunit zstd liblz4-tool file locales libacl1
 yes | sudo locale-gen en_US.UTF-8
+
+
+# Configuring network booting for a development setup
+yes | sudo apt-get install tftpd-hpa
