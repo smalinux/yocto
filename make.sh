fe25147d4cc372687bcc24e2e38621d5e2f7fc7b
Sohaib Mohamed <sohaib.amhmd@gmail.com>
2025-01-28 06:46:30 +0100

    `./make.sh`: Wrapper around bitbake <image>
    - Invoke bitbake <image>
    - rsync rootfs to /mnt/_OUTPUT/rootfs/<hardware>/target
    
    Signed-off-by: Sohaib Mohamed <sohaib.amhmd@gmail.com>

---
+++ make.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Settings
+# --------------
+THIS_FILE=$(realpath "$(dirname "${BASH_SOURCE[0]}")")
+IMAGE="core-image-bbb"
+
+
+#1
+bitbake $IMAGE
+#2
+bash $THIS_FILE/scripts/rootfs_nfs_sync.sh
