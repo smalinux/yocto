1f4dd1fc1de6125090d74bf4465a8c0996ca6a71
Sohaib Mohamed <sohaib.amhmd@gmail.com>
2025-01-28 06:38:26 +0100

    sudo ./scripts/serial_console.sh /dev/ttyUSB0
    Signed-off-by: Sohaib Mohamed <sohaib.amhmd@gmail.com>

--- scripts/serial_console.sh
+++ scripts/serial_console.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
 #
 
-picocom -b 115200 /dev/ttyUSB0
+#TODO $ ln this  script  under /bin
+DRIVE=${1}
+SPEED=115200
+
+# check if the SD-Card not exist...
+if [ -z ${DRIVE} ]; then
+   echo -e "\e[31mVar \${1} is empty/not exist... (ex: /dev/ttyUSB0)\e[0m"
+   exit 0
+fi
+
+
+picocom -b ${SPEED} ${DRIVE}
