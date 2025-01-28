cdf22a5542045b4975020655d7da8fda27e5786c
Sohaib Mohamed <sohaib.amhmd@gmail.com>
2025-01-28 06:31:58 +0100

    source ./env.sh
    Wrapper around poky/oe-init-build-env
    
    Signed-off-by: Sohaib Mohamed <sohaib.amhmd@gmail.com>

---
+++ env.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Settings
+# --------------
+THIS_FILE=$(realpath "$(dirname "${BASH_SOURCE[0]}")")
+
+
+# FIXUP Ubuntu 24
+#sudo echo 0 > /proc/sys/kernel/apparmor_restrict_unprivileged_userns
+
+source $THIS_FILE/poky/oe-init-build-env \
+   $THIS_FILE/build_bbb/
+cd ..
