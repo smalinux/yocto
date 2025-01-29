37cb09b61c85ce405f6d2cd5c83223c8e8a5b528
Sohaib Mohamed <sohaib.amhmd@gmail.com>
2025-01-29 17:09:04 +0100

    mv `build_bbb/` config dir to `build/beaglebone/`
    Signed-off-by: Sohaib Mohamed <sohaib.amhmd@gmail.com>

--- env.sh
+++ env.sh
@@ -9,5 +9,5 @@ THIS_FILE=$(realpath "$(dirname "${BASH_SOURCE[0]}")")
 #sudo echo 0 > /proc/sys/kernel/apparmor_restrict_unprivileged_userns
 
 source $THIS_FILE/poky/oe-init-build-env \
-   $THIS_FILE/build_bbb/
-cd ..
+   $THIS_FILE/build/beaglebone/
+cd $THIS_FILE
