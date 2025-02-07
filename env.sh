3b3f2d772b789663725d32386b9caa7dac502bcf
Sohaib Mohamed <sohaib.amhmd@gmail.com>
2025-02-07 23:13:47 +0100

    env.sh: update
    Signed-off-by: Sohaib Mohamed <sohaib.amhmd@gmail.com>

--- env.sh
+++ env.sh
@@ -3,11 +3,11 @@
 # Settings
 # --------------
 THIS_FILE=$(realpath "$(dirname "${BASH_SOURCE[0]}")")
+MACHINE=bbb
 
 
 # FIXUP Ubuntu 24
 #sudo echo 0 > /proc/sys/kernel/apparmor_restrict_unprivileged_userns
 
-source $THIS_FILE/poky/oe-init-build-env \
-   $THIS_FILE/build/beaglebone/
+source $THIS_FILE/poky/oe-init-build-env $THIS_FILE/build/$MACHINE/
 cd $THIS_FILE
